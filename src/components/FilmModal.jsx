5d3d6e0d02a1dc1b12e3b6c4eebe693606c8e11e
Serezha <volos11223344@yandex.ru>
2024-03-16 00:11:32 +0300

    добавляет модалку

--- src/components/FilmModal.jsx
+++ src/components/FilmModal.jsx
@@ -1,64 +1,58 @@
+import React, { useState } from 'react';
+
 function FilmModal(props) {
-  console.log(props, "...propsFilmModal");
+	const [spanOpen, setSpanOpen] = useState(false);
 
-  const { movieModalInfo, closeModal } = props;
+	function setSpanOpenShow() {
+		setSpanOpen(!spanOpen);
+	}
 
-  console.log(movieModalInfo, "...propsFilmModalinfoFilm");
-  const infoFilm = movieModalInfo.data;
+	const { movieModalInfo, closeModal } = props;
+	const infoFilm = movieModalInfo.data;
 
-  return (
-    <>
-      {infoFilm && (
-        <div className="modal__overlay">
-          <div className="modal__card">
-            <span className="modal__button-close-icon" onClick={closeModal}>
-              X
-            </span>
-            <img
-              className="modal__movie-backdrop"
-              src={infoFilm.posterUrl}
-              alt=""
-            />
-            <h2>
-              <span className="modal__movie-title">
-                Название {infoFilm.nameRu}
-              </span>
-              <span className="modal__movie-release-year">
-                Год {infoFilm.year}
-              </span>
-            </h2>
-            <ul className="modal__movie-info">
-              <div className="loader"></div>
-              {infoFilm.filmLength ? (
-                <li className="modal__movie-runtime">
-                  Время {infoFilm.filmLength}-минут
-                </li>
-              ) : (
-                ""
-              )}
-              <li>
-                Сайт:{" "}
-                <a className="modal__movie-site" href={infoFilm.webUrl}>
-                  {infoFilm.webUrl}
-                </a>
-              </li>
-              {infoFilm.genres.map((el, index) => (
-                <li className="modal__genre-item" key={index}>
-                  {el.genre}
-                </li>
-              ))}
-              <li className="modal__movie-overview">
-                Описание - {infoFilm.description}
-              </li>
-            </ul>
-            <button type="button" className="modal__button-close">
-              Закрыть
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{infoFilm && (
+				<div className="modal__overlay">
+					<div className="modal__card">
+						<span className="modal__button-close-icon" onClick={closeModal}>
+							X
+						</span>
+						<img className="modal__movie-backdrop" src={infoFilm.posterUrl} alt="" />
+						<h2 className="modal__title">
+							<span className="modal__movie-title">Название {infoFilm.nameRu}</span>
+							<span className="modal__movie-release-year">Год {infoFilm.year}</span>
+						</h2>
+						<ul className="modal__movie-info">
+							<div className="loader"></div>
+							{/* {infoFilm.filmLength ? <li className="modal__movie-runtime">Время {infoFilm.filmLength}-минут</li> : ''} */}
+							<li>
+								Сайт:{' '}
+								<a className="modal__movie-site" href={infoFilm.webUrl}>
+									{infoFilm.webUrl}
+								</a>
+							</li>
+							<li className="modal__genres">
+								{' '}
+								Жанры:
+								{infoFilm.genres.map((el, index) => (
+									<li className="modal__genre-item" key={index}>
+										{el.genre}
+									</li>
+								))}
+							</li>
+							<li className="modal__movie-overview">
+								<span className="modal__movie-overview-show" onClick={setSpanOpenShow}>
+									{spanOpen ? 'Свернуть описание' : 'Развернуть описание'}
+								</span>
+								{spanOpen && <span>Описание - {infoFilm.description}</span>}
+							</li>
+						</ul>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default FilmModal;
