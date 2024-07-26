0d14b2d3e9fe51852204c4c46b7656eb1e358597
Serezha <volos11223344@yandex.ru>
2024-07-26 12:03:30 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,50 @@
-import React, { useContext } from "react";
-import Movies from "./components/Movies";
-import Header from "./components/Header/Header";
-import Preloader from "./components/Preloader";
-import FilmModal from "./components/FilmModal";
-import PaginationBlock from "./components/Pagination/Pagination";
+import React, { useContext } from 'react';
+import Movies from './components/Movies';
+import Header from './components/Header/Header';
+import Preloader from './components/Preloader';
+import FilmModal from './components/FilmModal';
+import PaginationBlock from './components/Pagination/Pagination';
 // import Pagination from './components/Pagination';
-import { MovieContext } from "./components/context";
-import SwiperSlider from "./components/Slider/Slider";
-import SliderPreloader from "./components/SliderPreloader/SliderPreloader";
+import { MovieContext } from './components/context';
+import SwiperSlider from './components/Slider/Slider';
+import SliderPreloader from './components/SliderPreloader/SliderPreloader';
 // import { CircularProgress } from "@mui/material";
-import "./App.css";
+import './App.css';
 
 function App() {
-  const { movieArr, timerSlider, isLoaded, movieModalInfo, modalOpen, closeModal, searchMovie, showModal, setMovieModalInfo } = useContext(MovieContext);
+	const { movieArr, timerSlider, isLoaded, movieModalInfo, modalOpen, closeModal, searchMovie, showModal, setMovieModalInfo } = useContext(MovieContext);
 
-  console.log(timerSlider, "...timerSlider");
+	console.log(timerSlider, '...timerSlider');
 
-  return (
-    <div className="App">
-      <div className="container">
-        {modalOpen && <FilmModal movieModalInfo={movieModalInfo} closeModal={closeModal} />}
-        {isLoaded ? (
-          <>
-            <div className="content">
-              <Header searchFunc={searchMovie} />
-              <div>
-                <div style={{ display: timerSlider ? "block" : "none" }}>
-                  <SwiperSlider />
-                </div>
-                {/* <div className="sliderPreloader" style={{ display: timerSlider ? "none" : "flex" }}>
-                  <CircularProgress />
-                </div> */}
-                <div style={{ display: timerSlider ? "none" : "flex" }}>
-                  <SliderPreloader />
-                </div>
-              </div>
-              <Movies movieArr={movieArr} showModal={showModal} setMovieModalInfo={setMovieModalInfo} />
-            </div>
-            <PaginationBlock />
-            {/* /*<Pagination movieArr={movieArr} getPageMovie={getPageMovie} />*/}
-          </>
-        ) : (
-          <div className="loader">
-            <Preloader />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				{modalOpen && <FilmModal movieModalInfo={movieModalInfo} closeModal={closeModal} />}
+				{isLoaded ? (
+					<>
+						<div className="content">
+							<Header searchFunc={searchMovie} />
+							<div>
+								<div style={{ display: timerSlider ? 'block' : 'none' }}>
+									<SwiperSlider />
+								</div>
+								<div style={{ display: timerSlider ? 'none' : 'flex' }}>
+									<SliderPreloader />
+								</div>
+							</div>
+							<Movies movieArr={movieArr} showModal={showModal} setMovieModalInfo={setMovieModalInfo} />
+						</div>
+						<PaginationBlock />
+						{/* /*<Pagination movieArr={movieArr} getPageMovie={getPageMovie} />*/}
+					</>
+				) : (
+					<div className="loader">
+						<Preloader />
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
