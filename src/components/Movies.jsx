53deee581f06e670d023a1da69827eef891566e3
s.potapov <s.potapov@severnaya.ru>
2024-09-05 18:17:44 +0300

    likes working

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,39 +1,42 @@
-import { useState, useEffect } from 'react';
-import Card from './Card/Card';
+import { useState, useEffect } from "react";
+import Card from "./Card/Card";
 
 function Movies(props) {
-	const { movieArr, showModal, setMovieModalInfo } = props;
+  const { movieArr, showModal, setMovieModalInfo } = props;
 
-	const [favoriteArray, setFavoriteArray] = useState([]);
+  const [favoriteArray, setFavoriteArray] = useState([]);
 
-	useEffect(() => {
-		console.log(movieArr, '...movieArr in useEf');
+  useEffect(() => {
+    console.log(movieArr, "...movieArr in useEf");
 
-		let arrLS = localStorage.getItem('favoriteArray') !== null ? JSON.parse(localStorage.getItem('favoriteArray')) : [];
-		if (arrLS) {
-			setFavoriteArray(
-				movieArr.map((item) => {
-					const idExist = arrLS.some((itemLS) => itemLS.id == item.filmId);
-					if (idExist) {
-						return { ...item, isLiked: true };
-					} else {
-						return { ...item, isLiked: false };
-					}
-				}),
-			);
-		} else {
-			setFavoriteArray(movieArr);
-		}
-	}, [movieArr]);
+    let arrLS = localStorage.getItem("favoriteArray") !== null ? JSON.parse(localStorage.getItem("favoriteArray")) : [];
 
-	return (
-		<div style={{ marginTop: '50px' }} className="movies">
-			{movieArr.length > 0 &&
-				favoriteArray.map((film) => {
-					return <Card key={film.filmId} film={film} showModal={showModal} setMovieModalInfo={setMovieModalInfo} />;
-				})}
-		</div>
-	);
+    console.log(arrLS, "arrls in UE");
+
+    if (arrLS) {
+      setFavoriteArray(
+        movieArr.map((item) => {
+          const idExist = arrLS.some((itemLS) => itemLS.filmId == item.filmId);
+          if (idExist) {
+            return { ...item, isLiked: true };
+          } else {
+            return { ...item, isLiked: false };
+          }
+        })
+      );
+    } else {
+      setFavoriteArray(movieArr);
+    }
+  }, [movieArr]);
+
+  return (
+    <div style={{ marginTop: "50px" }} className="movies">
+      {movieArr.length > 0 &&
+        favoriteArray.map((film) => {
+          return <Card key={film.filmId} film={film} showModal={showModal} setMovieModalInfo={setMovieModalInfo} />;
+        })}
+    </div>
+  );
 }
 
 export default Movies;
--- src/components/context.jsx
+++ src/components/context.jsx
@@ -1,182 +1,186 @@
-import React, { createContext, useState, useEffect } from 'react';
-import useSessionStorage from '../hooks/useSessionStorage.jsx';
-import { API_Key, API_NEWS, API_url_popular, API_url_search, API_Movie_details } from '../constants.js';
+import React, { createContext, useState, useEffect } from "react";
+import useSessionStorage from "../hooks/useSessionStorage.jsx";
+import { API_Key, API_NEWS, API_url_popular, API_url_search, API_Movie_details } from "../constants.js";
 
 export const MovieContext = createContext();
 
 export const MovieDataContext = ({ children }) => {
-	const [movieArr, setMovieArr] = useState([]);
-	const [movieArrInfo, setMovieArrInfo] = useState('');
-	const [isLoaded, setIsLoaded] = useState(false);
-	const [movieModalInfo, setMovieModalInfo] = useState([]);
-	const [modalOpen, setModalOpen] = useState(false);
-	const [sliderArray, setSliderArray] = useState([]);
-	const [newsData, setNewsData] = useState([]);
-	const [timerSlider, setTimerSlider] = useState(false);
-	const [isHidedForm, setisHidedForm] = useState(false);
-	const [localState, setLocalState] = useSessionStorage('promo');
+  const [movieArr, setMovieArr] = useState([]);
+  const [movieArrInfo, setMovieArrInfo] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [movieModalInfo, setMovieModalInfo] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [sliderArray, setSliderArray] = useState([]);
+  const [newsData, setNewsData] = useState([]);
+  const [timerSlider, setTimerSlider] = useState(false);
+  const [isHidedForm, setisHidedForm] = useState(false);
+  const [localState, setLocalState] = useSessionStorage("promo");
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const popularResponse = await fetch(API_url_popular, {
-					headers: {
-						'Content-Type': 'application/json',
-						'X-API-KEY': API_Key,
-					},
-				});
-				const popularData = await popularResponse.json();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const popularResponse = await fetch(API_url_popular, {
+          headers: {
+            "Content-Type": "application/json",
+            "X-API-KEY": API_Key,
+          },
+        });
+        const popularData = await popularResponse.json();
 
-				setMovieArrInfo(popularData);
+        setMovieArrInfo(popularData);
 
-				let { films } = popularData;
+        let { films } = popularData;
 
-				let filmsWithLikes = films.map((filmItem) => {
-					return { ...filmItem, isLiked: false };
-				});
+        let filmsWithLikes = films.map((filmItem) => {
+          return { ...filmItem, isLiked: false };
+        });
 
-				console.log(films, '...updatedFilms...');
+        console.log(films, "...updatedFilms...");
 
-				const newsResponse = await fetch(API_NEWS, {
-					headers: {
-						'Content-Type': 'application/json',
-						'X-API-KEY': API_Key,
-					},
-				});
-				const newsData = await newsResponse.json();
+        const newsResponse = await fetch(API_NEWS, {
+          headers: {
+            "Content-Type": "application/json",
+            "X-API-KEY": API_Key,
+          },
+        });
+        const newsData = await newsResponse.json();
 
-				setMovieArr(filmsWithLikes);
+        setMovieArr(filmsWithLikes);
 
-				setNewsData(newsData);
+        setNewsData(newsData);
 
-				setIsLoaded(true);
-				setSliderArray(newsData?.items.slice(0, 5));
+        setIsLoaded(true);
+        setSliderArray(newsData?.items.slice(0, 5));
 
-				const timerOne = setTimeout(() => {
-					setTimerSlider(true);
-				}, 3000);
+        const timerOne = setTimeout(() => {
+          setTimerSlider(true);
+        }, 3000);
 
-				return () => clearTimeout(timerOne);
-			} catch (error) {
-				console.error('Error fetching data:', error);
-			}
-		};
+        return () => clearTimeout(timerOne);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-		if (localState === 'true') {
-			fetchData();
-		} else {
-			const timer = setTimeout(() => {
-				fetchData();
-				setLocalState('true');
-			}, 4000);
-			return () => clearTimeout(timer);
-		}
-	}, [localState]);
+    if (localState === "true") {
+      fetchData();
+    } else {
+      const timer = setTimeout(() => {
+        fetchData();
+        setLocalState("true");
+      }, 4000);
+      return () => clearTimeout(timer);
+    }
+  }, [localState]);
 
-	useEffect(() => {
-		document.addEventListener('click', (e) => {
-			if (!e.target.closest('.modal__card') && !e.target.closest('.movie')) {
-				setModalOpen(false);
-				document.body.style.overflow = 'auto';
-			}
-		});
-	}, [modalOpen]);
+  useEffect(() => {
+    document.addEventListener("click", (e) => {
+      if (!e.target.closest(".modal__card") && !e.target.closest(".movie")) {
+        setModalOpen(false);
+        document.body.style.overflow = "auto";
+      }
+    });
+  }, [modalOpen]);
 
-	function setLike(id) {
-		const likedArray = movieArr.map((item) => {
-			if (item.filmId == id) {
-				console.log(item, 'item');
-				return { ...item, isLiked: !item.isLiked };
-			} else {
-				return item;
-			}
-		});
-		setMovieArr(likedArray);
+  function setLike(id) {
+    const likedArray = movieArr.map((item) => {
+      if (item.filmId == id) {
+        console.log(item, "item");
+        return { ...item, isLiked: !item.isLiked };
+      } else {
+        return item;
+      }
+    });
+    setMovieArr(likedArray);
 
-		updateLS(id);
-	}
+    updateLS(id);
+  }
 
-	function updateLS(id) {
-		let arrLS = localStorage.getItem('favoriteArray') !== null ? JSON.parse(localStorage.getItem('favoriteArray')) : [];
+  function updateLS(id) {
+    let arrLS = localStorage.getItem("favoriteArray") !== null ? JSON.parse(localStorage.getItem("favoriteArray")) : [];
 
-		if (arrLS) {
-			let idExistInArr = arrLS.some((item) => item.id == id);
-			if (!idExistInArr) {
-				arrLS = [...arrLS, { id }];
-			} else {
-				arrLS = arrLS.filter((item) => item.id !== id);
-			}
-		}
+    if (arrLS) {
+      let idExistInArr = arrLS.some((item) => item.filmId == id);
+      if (!idExistInArr) {
+        const targetFilm = movieArr.find((item) => item.filmId == id);
+        console.log(targetFilm, "targetFilm");
+        // targetFilm.isLiked = true;
+        arrLS = [...arrLS, targetFilm];
+      } else {
+        arrLS = arrLS.filter((item) => item.filmId !== id);
+      }
+    }
 
-		localStorage.setItem('favoriteArray', JSON.stringify(arrLS));
-	}
+    localStorage.setItem("favoriteArray", JSON.stringify(arrLS));
+  }
 
-	function searchMovie(value) {
-		const apiSearcUrl = `${API_url_search}${value}`;
+  function searchMovie(value) {
+    const apiSearcUrl = `${API_url_search}${value}`;
 
-		fetch(value ? apiSearcUrl : API_url_popular, {
-			headers: {
-				'Content-Type': 'application.json',
-				'X-API-KEY': API_Key,
-			},
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data, 'dataSearch');
-				setMovieArr(data.films);
-			});
-	}
+    fetch(value ? apiSearcUrl : API_url_popular, {
+      headers: {
+        "Content-Type": "application.json",
+        "X-API-KEY": API_Key,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data, "dataSearch");
+        setMovieArr(data.films);
+      });
+  }
 
-	function showModal(e, filmInfo) {
-		if (!e.target.closest('.like')) {
-			fetch(API_Movie_details + filmInfo, {
-				headers: {
-					'Content-Type': 'application.json',
-					'X-API-KEY': API_Key,
-				},
-			})
-				.then((response) => response.json())
-				.then((data) => {
-					setMovieModalInfo(data);
-					document.body.style.overflow = 'hidden';
-					setModalOpen(true); // Закрываем модальное окно
-				});
-		} else return;
-	}
+  function showModal(e, filmInfo) {
+    if (!e.target.closest(".like")) {
+      fetch(API_Movie_details + filmInfo, {
+        headers: {
+          "Content-Type": "application.json",
+          "X-API-KEY": API_Key,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setMovieModalInfo(data);
+          document.body.style.overflow = "hidden";
+          setModalOpen(true); // Закрываем модальное окно
+        });
+    } else return;
+  }
 
-	function closeModal() {
-		setModalOpen(false); // Закрываем модальное окно
-		document.body.style.overflow = 'auto';
-	}
+  function closeModal() {
+    setModalOpen(false); // Закрываем модальное окно
+    document.body.style.overflow = "auto";
+  }
 
-	return (
-		<MovieContext.Provider
-			value={{
-				movieArr,
-				isLoaded,
-				movieModalInfo,
-				modalOpen,
-				setMovieArr,
-				setIsLoaded,
-				setMovieModalInfo,
-				setModalOpen,
-				searchMovie,
-				sliderArray,
-				localState,
-				showModal,
-				timerSlider,
-				setTimerSlider,
-				closeModal,
-				setLike,
-				// favoriteArray,
-				newsData,
-				movieArrInfo,
-				// setRemoveToFavoriteArray,
-				// removeFromFavoriteArray,
-				isHidedForm,
-				setisHidedForm,
-			}}>
-			{children}
-		</MovieContext.Provider>
-	);
+  return (
+    <MovieContext.Provider
+      value={{
+        movieArr,
+        isLoaded,
+        movieModalInfo,
+        modalOpen,
+        setMovieArr,
+        setIsLoaded,
+        setMovieModalInfo,
+        setModalOpen,
+        searchMovie,
+        sliderArray,
+        localState,
+        showModal,
+        timerSlider,
+        setTimerSlider,
+        closeModal,
+        setLike,
+        // favoriteArray,
+        newsData,
+        movieArrInfo,
+        // setRemoveToFavoriteArray,
+        // removeFromFavoriteArray,
+        isHidedForm,
+        setisHidedForm,
+      }}
+    >
+      {children}
+    </MovieContext.Provider>
+  );
 };
