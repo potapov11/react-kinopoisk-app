a3f2430ab0d08b0231b8eb4a5f8bed7acf6b5b1e
s.potapov <s.potapov@severnaya.ru>
2024-09-19 15:54:54 +0300

    added full logyc with form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,56 +1,93 @@
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 import style from "./style.css";
 
-function Form() {
+function Form(props) {
+  const [formState, setFormState] = useState("");
+  const { setOpenedFormModal } = props;
+
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
   } = useForm({
-    mode: "onChange", // Валидация при каждом изменении
+    mode: "onChange",
   });
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data, e) => {
+    const { user_email } = data;
+    const { user_name } = data;
+
+    const resObj = JSON.stringify({ user_email, user_name });
+
+    await fetch("https://api.openjavascript.info/post", {
+      method: "POST",
+      body: resObj,
+      headers: { "Content-type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const text = `Вы успешно подписаны.
+
+        Не забывайте проверять вашу почту ${user_name}`;
+        setFormState(text);
+
+        localStorage.setItem("form", true);
+
+        setTimeout(() => {
+          setOpenedFormModal(false);
+        }, 3000);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <h2>Если вы хотите получать информацию о премьерах, трейлерах, афишу - подпишитесь на наши обновления</h2>
-
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          {...register("user_name", {
-            required: "Это поле обязательно",
-            minLength: { value: 3, message: "Минимум 3 символа" },
-            pattern: {
-              value: /^[A-Za-zА-Яа-яЁё\s\-']+$/,
-              message: "Только буквы и пробелы",
-            },
-          })}
-        />
-        {errors.user_name && <p className="form__error">{errors.user_name.message}</p>}
-
-        <label htmlFor="mail">Email:</label>
-        <input
-          type="email"
-          id="mail"
-          {...register("user_email", {
-            required: "Это поле обязательно",
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: "Введите корректный email",
-            },
-          })}
-        />
-        {errors.user_email && <p className="form__error">{errors.user_email.message}</p>}
-
-        <button type="submit">
-          <button type="submit" disabled={!isValid} />
-          Отправить
-        </button>
-      </form>
+      {!formState ? (
+        <form className="form" noValidate onSubmit={handleSubmit(onSubmit)}>
+          <h2>Если вы хотите получать информацию о премьерах, трейлерах, афишу - подпишитесь на наши обновления</h2>
+          <div className="form__group-name">
+            <label htmlFor="name">Name:</label>
+            <input
+              // className={!isValid ? "outline-red" : ""}
+              type="text"
+              id="name"
+              {...register("user_name", {
+                required: "Это поле обязательно",
+                minLength: { value: 3, message: "Минимум 3 символа" },
+                maxLength: { value: 20, message: "Максимум 20 символов" },
+                pattern: {
+                  value: /^[A-Za-zА-Яа-яЁё\s\-']+$/,
+                  message: "Только буквы и пробелы",
+                },
+              })}
+            />
+            {errors.user_name && <p className="form__error">{errors.user_name.message}</p>}
+          </div>
+          <div className="form__group-email">
+            <label htmlFor="mail">Email:</label>
+            <input
+              // className={!isValid ? "outline-red" : ""}
+              type="email"
+              id="mail"
+              {...register("user_email", {
+                required: "Это поле обязательно",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Введите корректный email",
+                },
+              })}
+            />
+            {errors.user_email && <p className="form__error">{errors.user_email.message}</p>}
+          </div>
+          <input type="submit" className={!isValid ? "disabled" : ""} value="Отправить" />
+        </form>
+      ) : (
+        <div className="response-form">
+          <p>Вы успешно подписаны.</p>
+          <p>Не забывайте проверять вашу почту :)</p>
+        </div>
+      )}
     </>
   );
 }
--- src/components/FormModal/FormModal.jsx
+++ src/components/FormModal/FormModal.jsx
@@ -1,36 +1,36 @@
-import { useEffect, useState } from 'react';
-import Form from '../Form/Form';
-import style from './style.css';
+import { useEffect, useState } from "react";
+import Form from "../Form/Form";
+import style from "./style.css";
 // import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 function FormModal() {
-	const [openedFormModal, setOpenedFormModal] = useState(false);
+  const [openedFormModal, setOpenedFormModal] = useState(false);
 
-	function checkClick(e) {
-		const target = e.target;
-		if (!target.closest('form')) {
-			setOpenedFormModal(false);
-		}
-	}
+  function checkClick(e) {
+    const target = e.target;
+    if (!target.closest("form")) {
+      setOpenedFormModal(false);
+    }
+  }
 
-	useEffect(() => {
-		let timer = setTimeout(() => {
-			setOpenedFormModal(true);
-		}, 5000);
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setOpenedFormModal(true);
+    }, 5000);
 
-		return () => {
-			clearTimeout(timer);
-		};
-	}, []);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
-	return (
-		openedFormModal && (
-			<div className="wrapper-modal" onClick={(e) => checkClick(e)}>
-				<div className="close" onClick={() => setOpenedFormModal(false)}></div>
-				<Form />
-			</div>
-		)
-	);
+  return (
+    openedFormModal && (
+      <div className="wrapper-modal" onClick={(e) => checkClick(e)}>
+        <div className="close" onClick={() => setOpenedFormModal(false)}></div>
+        <Form setOpenedFormModal={setOpenedFormModal} />
+      </div>
+    )
+  );
 }
 
 export default FormModal;
